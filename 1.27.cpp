e8e6a3dbb951b37b02068e36bfc82df2d74d667e
nstshhvcv <nstshhvcv@mail.ru>
2025-05-27 12:14:05 +0300

    Add files via upload

---
+++ 1.27.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath> // для sqrt()
+
+int main() {
+    const double R = 6350.0; // радиус Земли в км
+    double h; // высота над поверхностью (в км)
+
+    std::cout << "Введите высоту над поверхностью Земли (км): ";
+    std::cin >> h;
+
+    
+    double distance = sqrt(pow(R + h, 2) - pow(R, 2));
+
+    std::cout << "Расстояние до горизонта: " << distance << " км" << std::endl;
+
+    return 0;
+}
